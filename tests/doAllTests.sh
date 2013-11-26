05a5e05a609d532407c565f116f100ba04403663
Duarte Barreira <duartemtbarreira@gmail.com>
2013-11-26 15:38:46 +0000

    Added the test cases to the repos

---
+++ tests/doAllTests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TESTS="test_kos_single_threaded_put_get test_kos_single_threaded_put_remove_get test_kos_single_threaded_put_get_put_get test_kos_single_threaded_put_get_remove_put_get test_kos_single_threaded_put_dump test_kos_single_threaded_put_get_remove_put_dump  test_kos_multi_threaded_put_get test_kos_multi_threaded_put_get_remove_get   test_kos_multi_threaded_all test_kos_multi_threaded_all_getAll test_kos_multi_threaded_all_shared"
+
+c=0
+for i in $TESTS
+do
+	(( c++ ))
+	echo #########################################################
+	echo Executing test $c: $i
+	echo #########################################################
+	./${i}
+	echo ---------------------------------------------------------
+	echo press a key to continue
+	read a
+done
